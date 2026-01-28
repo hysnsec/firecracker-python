47b8900a6bc7f6862a4c7a6b223a973acba6ea77
0xR35tu <156645474+0xR35tu@users.noreply.github.com>
2026-01-28 17:27:41 +0700

    Add local_scheme to use_scm_version in setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     use_scm_version={
         "write_to": "firecracker/_version.py",
         "version_scheme": "post-release",
+        "local_scheme": "no-local-version",
     },
     setup_requires=['setuptools_scm'],
 )
